def352edde405271296ff636d270927e71d5d377
Qoosim <ayinde.abdulghaniyy@gmail.com>
2023-07-30 22:04:36 +0100

    Add production link as the proxy for the baseUrl

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -11,12 +11,10 @@ const Posts = () => {
   const [editingPostId, setEditingPostId] = useState(null);
   const navigate = useNavigate();
 
-  console.log(posts);
-
   axios.defaults.headers.common[
     "Authorization"
   ] = `Bearer ${localStorage.getItem("token")}`;
-  // axios.defaults.baseURL = "http://localhost:7000";
+  // axios.defaults.baseURL = "http://localhost:5000";
 
   const fetchPosts = async () => {
     try {
